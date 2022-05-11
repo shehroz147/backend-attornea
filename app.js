8330b47872bfa6ccb05bc0786770827fabd48e36
Shehroz <shehroz.virk147@gmail.com>
2022-05-11 23:39:43 +0500

    heroku

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
-        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization, Content-Type, Signature"
     );
 
